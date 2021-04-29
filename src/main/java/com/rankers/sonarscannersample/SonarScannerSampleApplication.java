1e411e1893fb39697009c08dac36b27684592778
Richard Ankers <rankers@deloitte.co.uk>
2021-04-29 18:44:11 +0100

    Add code smell

--- src/main/java/com/rankers/sonarscannersample/SonarScannerSampleApplication.java
+++ src/main/java/com/rankers/sonarscannersample/SonarScannerSampleApplication.java
@@ -2,7 +2,7 @@ package com.rankers.sonarscannersample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.Map;
 @SpringBootApplication
 public class SonarScannerSampleApplication {
 
