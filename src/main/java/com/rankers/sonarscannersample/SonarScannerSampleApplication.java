07c142e647657b3fea34abc11149870806db7549
Richard Ankers <rankers@deloitte.co.uk>
2021-04-29 18:32:39 +0100

    init

---
+++ src/main/java/com/rankers/sonarscannersample/SonarScannerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.rankers.sonarscannersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarScannerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarScannerSampleApplication.class, args);
+	}
+
+}
